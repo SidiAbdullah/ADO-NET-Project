5c988556178ac69591c4657d1760a7b1505b2be4
Sidi Abdullah Lemrabott <sidiabdullah67@gmail.com>
2025-07-01 14:58:22 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using contactsBusinessLayer;
+
+namespace contactsPresantationLayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsContacts contact = clsContacts.Find(1);
+            if (contact != null)
+            {
+                contact.printContactInfos();
+            }
+            else
+            {
+                Console.WriteLine("contact [" + 1 + "] is not found");
+            }
+        }
+    }
+}
